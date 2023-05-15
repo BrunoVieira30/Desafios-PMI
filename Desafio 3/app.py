a51df754000a9d8e61784e82e349cf1075a7615d
BrunoVieira30 <bruno.vieira30@fatec.sp.gov.br>
2023-05-15 17:13:12 -0300

    Conecta form ao banco

--- Desafio 3/app.py
+++ Desafio 3/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 import mysql.connector
 
 app = Flask(__name__)
@@ -25,11 +25,21 @@ def contato():
     if request.method == 'GET':
         return render_template('contato.html', background= url_for('static', filename='imagens/fundo2.png'))
     else:
-        comentarios_teste = [request.form, request.form]
-        return render_template('comentarios.html', comentarios=comentarios_teste, background= url_for('static', filename='imagens/fundo2.png'))
+        comentario = request.form
+        cursor.execute(f"INSERT INTO comentarios (email, assunto, descricao) VALUES ('{comentario['email']}', '{comentario['assunto']}', '{comentario['descricao']}')")
+        database.commit()
+        
+        return redirect('/comentarios')
+
+@app.route('/comentarios')
+def comentarios():
+    cursor.execute("SELECT * FROM comentarios")
+    comentarios = cursor.fetchall()
+    return render_template('comentarios.html', comentarios=comentarios, background= url_for('static', filename='imagens/fundo2.png'))
 
 @app.route('/quemsomos')
 def quemsomos():
     return render_template('quem_somos.html', background= url_for('static', filename='imagens/fundo2.png'))
 
 app.run(debug=True)
+cursor.close()
