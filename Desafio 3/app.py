e8225ab82d749ac69a8a70a72d0762d0eb177c71
BrunoVieira30 <bruno.vieira30@fatec.sp.gov.br>
2023-05-15 16:20:12 -0300

    Cria conexão com o banco

--- Desafio 3/app.py
+++ Desafio 3/app.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template, url_for
+import mysql.connector
 
 app = Flask(__name__)
 
+# MySQL
+database = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root"
+)
+
+cursor = database.cursor()
+
+cursor.execute("CREATE DATABASE IF NOT EXISTS desafio3")
+cursor.execute("USE desafio3")
 
 @app.route('/')
 def index():
     return render_template('index.html', background= url_for('static', filename='imagens/fundo.png'))
 
-@app.route('/contato')
+@app.route('/contato', methods=['GET', 'POST'])
 def contato():
     return render_template('contato.html', background= url_for('static', filename='imagens/fundo2.png'))
 
